78e19fa5da29317cde9225843ecc32e9c6d23bec
AtomicJew <mshneyder0@gmail.com>
2019-10-01 18:14:31 -0400

    My name Jeff

--- liquidox/ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTankMode.java
+++ liquidox/ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTankMode.java
@@ -9,12 +9,21 @@ import org.firstinspires.ftc.teamcode.LO2Library;
 
 @TeleOp
 public class BasicTankMode extends OpMode {
-
+    DcMotor lf, lb, rf, rb;
     public void init() {
+        lf = hardwareMap.dcMotor.get("lf");
+        lb = hardwareMap.dcMotor.get("lb");
+        rf = hardwareMap.dcMotor.get("rf");
+        rb = hardwareMap.dcMotor.get("rb");
+    }
 
-    }
     public void loop() {
+        lf = -(gamepad1.leftstick_y);
+        lb = -(gamepad1.leftstick_y);
+        rf = -(gamepad1.leftstick_y);
+        rb = -(gamepad1.leftstick_y);
     }
+}
 
 
 
