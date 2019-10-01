ecae6e2e28dca2f33206ca0a2d37cf0235e2cdd1
Iglefan <jh231@students.needham.k12.ma.us>
2019-10-01 18:31:45 -0400

    Oct 1 Start of Meeting

--- liquidox/ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTankMode.java
+++ liquidox/ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTankMode.java
@@ -10,12 +10,25 @@ import org.firstinspires.ftc.teamcode.LO2Library;
 @TeleOp
 public class BasicTankMode extends OpMode {
 
+    DcMotor lf, lb, rf, rb;
     public void init() {
-
+        lf = hardwareMap.dcMotor.get ("lf");
+        lb = hardwareMap.dcMotor.get ("lb");
+        rf = hardwareMap.dcMotor.get ("rf");
+        rb = hardwareMap.dcMotor.get ("rb");
     }
     public void loop() {
-    }
+                lf.setPower = -(gamepad1.left_stick_y);
+                lb.setPower = -(gamepad1.left_stick_y);
+                rf.setPower = (gamepad1.left_stick_y);
+                rb.setPower = (gamepad1.left_stick_y);
 
+                lf.setPower = (gamepad1.left_stick_x);
+                lb.setPower = (gamepad1.left_stick_x);
+                rf.setPower = -(gamepad1.left_stick_x);
+                rb.setPower = -(gamepad1.left_stick_x);
+    }
+}
 
 
 
