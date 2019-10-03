f944bf238f0f4ea805c56a6e5d7b971f063dfeee
AtomicJew <mshneyder0@gmail.com>
2019-10-03 15:02:52 -0400

    Added Drive

--- liquidox/ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTankMode.java
+++ liquidox/ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTankMode.java
@@ -7,9 +7,19 @@ import com.qualcomm.robotcore.util.Range;
 import org.firstinspires.ftc.teamcode.LO2Library;
 
 
+
 @TeleOp
 public class BasicTankMode extends OpMode {
     DcMotor lf, lb, rf, rb;
+
+    public void drive (float lf, float lb, float rf, float rb){
+        lf.setPower(-lf);
+        lb.setPower(-lb);
+        rf.setPower(rf);
+        rb.setPower(rb);        
+
+    }
+
     public void init() {
         lf = hardwareMap.dcMotor.get("lf");
         lb = hardwareMap.dcMotor.get("lb");
