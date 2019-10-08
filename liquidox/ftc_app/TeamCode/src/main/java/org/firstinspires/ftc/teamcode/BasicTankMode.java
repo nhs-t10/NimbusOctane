b90789e151273a8e3711d9aa6de7907fcb560cfc
jesusisfromcleveland <tml231@students.needham.k12.ma.us>
2019-10-08 15:46:49 -0400

    carry

--- liquidox/ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTankMode.java
+++ liquidox/ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTankMode.java
@@ -10,6 +10,14 @@ import org.firstinspires.ftc.teamcode.LO2Library;
 @TeleOp
 public class BasicTankMode extends OpMode {
     DcMotor lf, lb, rf, rb;
+    Public void drive (float lf, float lb, float rf, float rb){
+        lf.setPower(-lf);
+        lb.setPower(-lb);
+        rf.setPower(rf);
+        rb.setPower(rb);
+    }
+
+
     public void init() {
         lf = hardwareMap.dcMotor.get("lf");
         lb = hardwareMap.dcMotor.get("lb");
@@ -18,15 +26,28 @@ public class BasicTankMode extends OpMode {
     }
 
     public void loop() {
-        lf = -(gamepad1.leftstick_y);
-        lb = -(gamepad1.leftstick_y);
-        rf = -(gamepad1.leftstick_y);
-        rb = -(gamepad1.leftstick_y);
+        float lX = gamepad1.leftstick_x;
+        float lY = gamepad1.leftstick_y;
+        float rX = gamepad1.rightstick_x;
+ //       drive(lY, lY, lY, lY);
+        if (lf == lb) {
+            rX.setPower(lf);
+        }
+        if (lb == lf) {
+            rX.setPower(lb);
+        }
+        if (rf == rb) {
+            rX.setPower(rf);
+        }
+        if (rb == rf) {
+            rX.setPower(rb);
+        }
+
+        drive(rX, rX, rY, rY);
     }
 
-    //eric test
 }
-hi
+
 
 
 
