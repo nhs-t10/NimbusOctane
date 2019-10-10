498b65950e66d590cc7223755f53ddd629eaa0cd
AtomicJew <mshneyder0@gmail.com>
2019-10-10 16:23:17 -0400

    Im the best

--- liquidox/ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTankMode.java
+++ liquidox/ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTankMode.java
@@ -11,12 +11,14 @@ import org.firstinspires.ftc.teamcode.LO2Library;
 @TeleOp
 public class BasicTankMode extends OpMode {
     DcMotor lf, lb, rf, rb;
+    float [] omniValues = new float[4];
 
-    public void drive (float lf, float lb, float rf, float rb){
-        lf.setPower(-lf);
-        lb.setPower(-lb);
-        rf.setPower(rf);
-        rb.setPower(rb);        
+
+    public void drive (float Lf, float Lb, float Rf, float Rb){
+        lf.setPower(-Lf);
+        lb.setPower(-Lb);
+        rf.setPower(Rf);
+        rb.setPower(Rb);
 
     }
 
@@ -31,9 +33,18 @@ public class BasicTankMode extends OpMode {
        float lX = gamepad1.left_stick_x;
        float lY = gamepad1.left_stick_y;
        float rX = gamepad1.right_stick_x;
-       drive(lY, lY, lY, lY);
+       omniValues[0] = (lY + rX - lX)/3;
+       omniValues[1] = (lY + rX + lX)/3;
+       omniValues[2] = (lY - rX - lX)/3;
+       omniValues[3] = (lY - rX + lX)/3;
+       drive(omniValues[0], omniValues[1], omniValues[2], omniValues[3]);
+       
     }
+
+
 }
+
+
 
 
 
