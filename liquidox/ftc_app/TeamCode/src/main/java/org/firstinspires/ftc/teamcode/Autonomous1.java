44e70152af27ed093c5a42916851b0112e7d7ebf
moekofi <43388005+moekofi@users.noreply.github.com>
2019-11-05 19:12:26 -0500

    Autonomous Exists now

--- liquidox/ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous1.java
+++ liquidox/ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous1.java
@@ -7,21 +7,80 @@ import com.qualcomm.robotcore.util.Range;
 import org.firstinspires.ftc.teamcode.LO2Library;
 
 
-@Autonomous
-public class BasicTankMode extends OpMode {
+@TeleOp
+public class Autonomous1 extends OpMode {
     DcMotor lf, lb, rf, rb;
-
+    float [] omniValues = new float [4];
+    public float inchConversion(float inches){
+        return inches/4;
+    }
     public void drive(float x, float y, float h, float k){
-        lf.setPower(-x);
-        lb.setPower(-y);
-        rf.setPower(h);
-        rb.setPower(k);
+        lf.setPower(-lf);
+        lb.setPower(-lb);
+        rf.setPower(rf);
+        rb.setPower(rb);
+    }
+    public void forward (float rotations) {
+        int position = lf.getCurrentPosition():
+        while lf.getCurrentPosition() < position - (rotations * 560) {
+            lf.setPower(-1);
+            lb.setPower(-1);
+            rf.setPower(1);
+            rb.setPower(1);
+        }
+        lf.setPower(0);
+        lb.setPower(0);
+        rf.setPower(0);
+        rb.setPower(0);
+    }
+    public void right (float rotations) {
+        int position = lf.getCurrentPosition():
+        while lf.getCurrentPosition() < position - (rotations * 560) {
+            lf.setPower(-1);
+            lb.setPower(-1);
+            rf.setPower(-1);
+            rb.setPower(-1);
+        }
+        lf.setPower(0);
+        lb.setPower(0);
+        rf.setPower(0);
+        rb.setPower(0);
+    }
+    public void left (float rotations) {
+        int position = lf.getCurrentPosition():
+        while lf.getCurrentPosition() < position + (rotations * 560) {
+            lf.setPower(1);
+            lb.setPower(1);
+            rf.setPower(1);
+            rb.setPower(1);
+        }
+        lf.setPower(0);
+        lb.setPower(0);
+        rf.setPower(0);
+        rb.setPower(0);
+    }
+    public void back (float rotations) {
+        int position = lf.getCurrentPosition():
+        while lf.getCurrentPosition() < position + (rotations * 560) {
+            lf.setPower(1);
+            lb.setPower(1);
+            rf.setPower(-1);
+            rb.setPower(-1);
+        }
+        lf.setPower(0);
+        lb.setPower(0);
+        rf.setPower(0);
+        rb.setPower(0);
     }
 
     public void init() {
-
+        lf = hardwareMap.dcMotor.get("lf");
+        lb = hardwareMap.dcMotor.get("lb");
+        rf = hardwareMap.dcMotor.get("rf");
+        rb = hardwareMap.dcMotor.get("rb");
     }
     public void loop() {
 
-
     }
+}
+
--- liquidox/ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTankMode.java
+++ liquidox/ftc_app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTankMode.java
@@ -10,12 +10,15 @@ import org.firstinspires.ftc.teamcode.LO2Library;
 @TeleOp
 public class BasicTankMode extends OpMode {
     DcMotor lf, lb, rf, rb;
-
+    float [] omniValues = new float [4];
+    public float inchConversion(float inches){
+        return inches/4;
+    }
     public void drive(float x, float y, float h, float k){
-        lf.setPower(-x);
-        lb.setPower(-y);
-        rf.setPower(h);
-        rb.setPower(k);
+        lf.setPower(-lf);
+        lb.setPower(-lb);
+        rf.setPower(rf);
+        rb.setPower(rb);
     }
 
 
@@ -30,11 +33,75 @@ public class BasicTankMode extends OpMode {
         float lY = gamepad1.leftstick_y;
         float lX = gamepad1.leftstick_x;
         float rX = gamepad1.rightstick_x;
-        drive (lY, lY, lY, lY);
+        omniValues [0] = lY + rX - lX;
+        omniValues [1] = lY + rX + lX;
+        omniValues [2] = lY - rX - lX;
+        onmiValues [3] = lY - rX + lX;
 
+        float highest = 0;
+        for (int i=0, i<4; i++) {
+            if(omniValues[i].abs > highest) {
+                highest = omniValues[i].abs;
+            }
+        }
+        if(highest > 1) {
+            for (int i = 0, i<4;i++){
+                omniValues[i] = omniValues[i] / highest;
+            }
+        }
+        drive(omniValues[0], omniValues[1], omniValues[2], onmiValues[3]);
     }
+}
 
-
-
-
-
+    public void forward (float rotations) {
+        int position = lf.getCurrentPosition():
+        while lf.getCurrentPosition() < position - (rotations * 560) {
+            lf.setPower(-1);
+            lb.setPower(-1);
+            rf.setPower(1);
+            rb.setPower(1);
+        }
+        lf.setPower(0);
+        lb.setPower(0);
+        rf.setPower(0);
+        rb.setPower(0);
+    }
+    public void right (float rotations) {
+        int position = lf.getCurrentPosition():
+        while lf.getCurrentPosition() < position - (rotations * 560) {
+            lf.setPower(-1);
+            lb.setPower(-1);
+            rf.setPower(-1);
+            rb.setPower(-1);
+        }
+        lf.setPower(0);
+        lb.setPower(0);
+        rf.setPower(0);
+        rb.setPower(0);
+    }
+    public void left (float rotations) {
+        int position = lf.getCurrentPosition():
+        while lf.getCurrentPosition() < position + (rotations * 560) {
+            lf.setPower(1);
+            lb.setPower(1);
+            rf.setPower(1);
+            rb.setPower(1);
+        }
+        lf.setPower(0);
+        lb.setPower(0);
+        rf.setPower(0);
+        rb.setPower(0);
+    }
+    public void back (float rotations) {
+        int position = lf.getCurrentPosition():
+        while lf.getCurrentPosition() < position + (rotations * 560) {
+            lf.setPower(1);
+            lb.setPower(1);
+            rf.setPower(-1);
+            rb.setPower(-1);
+        }
+        lf.setPower(0);
+        lb.setPower(0);
+        rf.setPower(0);
+        rb.setPower(0);
+    }
